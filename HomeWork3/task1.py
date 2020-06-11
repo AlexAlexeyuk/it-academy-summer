d287133f0ac42bb9395b6137ee994cb9b330b7bd
Homework1:Alexeyuk <canidoheliverda@gmail.com>
2020-06-11 21:35:51 +0300

    initial commit hw3

---
+++ HomeWork3/task1.py
@@ -0,0 +1,14 @@
+'''Напишите программу, которая печатает цифры от 1 до 100, но вместо чисел,
+ кратных 3 пишет Fizz, вместо чисел кратный 5 пишет Buzz, а вместо чисел
+ одновременно кратных и 3 и 5 - FizzBuzz'''
+enter = int(input('Enter any number: '))
+while enter < 100:
+  if not enter % 3 and enter % 5:
+    print('FizzBuzz')
+  elif not enter % 5 :
+    print('Buzz')
+  elif not enter % 3 :
+    print ('Fizz')
+  else:
+    print(enter)
+  enter += 1
