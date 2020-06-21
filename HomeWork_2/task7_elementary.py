e0709bb2fb29a2c48e7019b5e10e3013ae5a56eb
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-21 19:38:35 +0300

    task7-ele-edit

--- HomeWork_2/task7_elementary.py
+++ HomeWork_2/task7_elementary.py
@@ -7,19 +7,19 @@ i = 0
 num = int(input('enter an integer: '))
 if num:
     while not num % 10:
-            num = num // 10;
-            i += 1 
+            num = num // 10
+            i += 1
     print ("The number at the end has " + str(i) + ' zeros')
 if not num:
     print('The number at the end has 1 zero')
-    
+
 '''prints a tuple with 3 elements - 
 first, third and second to the last'''
 
 elements = tuple(input("Enter number after coma: "))
 print(tuple(elements[i] for i in [0, 2, -2]))
 
-'''prints the first word 
+'''prints the first word
 in a given text.'''
 
 pattern = r"[a-zA-Z']+"
