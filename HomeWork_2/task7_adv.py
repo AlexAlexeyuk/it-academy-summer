705df93e831a6c290c21c92ee7be4d6a7e723df0
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-21 19:37:04 +0300

    task_7_ad edit

--- HomeWork_2/task7_adv.py
+++ HomeWork_2/task7_adv.py
@@ -1,11 +1,10 @@
-'''If we list all the natural numbers below 10 that are multiples 
-of 3 or 5, we get 3, 5, 6 and 9. 
-The sum of these multiples is 23.
-Finish the solution so that it returns the sum of all the multiples 
-of 3 or 5 below the number passed in.
-Note: If the number is a multiple of both 3 and 5, only count it once.'''
-
+'''If we list all the natural numbers below 10 that are multiples
+of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples
+of 3 or 5 below the number passed in.Note: If the number is
+a multiple of both 3 and 5, only count it once.'''
 number = 14
+
 i = 3
 j = 5
 n_tree = 0
@@ -17,4 +16,3 @@ while j < number:
     n_five = n_five + j
     j += 5
 print(n_five + n_tree)
-    
