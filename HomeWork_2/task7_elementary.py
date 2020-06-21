0d69748e8c28c058f2f156231387b4352b2690aa
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-21 18:29:48 +0300

    task7_elem

--- HomeWork_2/task7_elementary.py
+++ HomeWork_2/task7_elementary.py
@@ -1,6 +1,8 @@
 import re
 
-#Need to print amout of zeros at the end of the integer
+'''returns amount of 0
+at the end of number'''
+
 i = 0
 num = int(input('enter an integer: '))
 if num:
@@ -11,11 +13,15 @@ if num:
 if not num:
     print('The number at the end has 1 zero')
     
-#prints a tuple with 3 elements - first, third and second to the last
-elements = tuple(input("Enter number after coma: "))
-print(tuple(elements[i] for i in [0,2,-2]))
+'''prints a tuple with 3 elements - 
+first, third and second to the last'''
 
-#prints the first word in a given text.
+elements = tuple(input("Enter number after coma: "))
+print(tuple(elements[i] for i in [0, 2, -2]))
+
+'''prints the first word 
+in a given text.'''
+
 pattern = r"[a-zA-Z']+"
 text = input("Enter a text: ")
 match = re.findall(pattern, text)
