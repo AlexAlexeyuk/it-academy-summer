2148fcfc92839191290b76abb4b6ef1829bdbe8b
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-21 19:33:42 +0300

    task5 edit

--- HomeWork_2/task5.py
+++ HomeWork_2/task5.py
@@ -1,16 +1,15 @@
 def fibonacci(n):
-    
+
     """Поиск числа фибоначчи.
     :param n: Номер числа Фибоначчи.
-    :return: Число. n-ое число Фибоначчи
-    """
+    :return: Число. n-ое число Фибоначчи"""
 
     n = int(input("enter number in order fibonacci: "))
     fib1 = fib2 = 1
     for element in range(3, n + 1):
       fib1, fib2 = fib2, fib1 + fib2
     return fib2
-     
+
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
     n = 0
