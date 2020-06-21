ff5ecec0021614998e7daec00afc3cc96c675dd4
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-21 18:11:34 +0300

    Task5*

--- HomeWork_2/task5.py
+++ HomeWork_2/task5.py
@@ -1,24 +1,16 @@
-"""Выведите n-ое число Фибоначчи, используя только временные переменные,
-    циклические операторы и условные операторы. n - вводится.
-"""
-
 def fibonacci(n):
     
     """Поиск числа фибоначчи.
     :param n: Номер числа Фибоначчи.
     :return: Число. n-ое число Фибоначчи
     """
-    
-    fib1 = 1
-    fib2 = 1
-    number_in_order = int(input("Enter order number of Fibonatchi: "))
-    i = 0
-    while i < number_in_order - 2:   # необходимое условие, т.к. первые два числа = 1
-        fib_sum = fib1 + fib2 
-        fib1 = fib2
-        fib2 = fib_sum   # присваиваем ф2 предыдущую сумму 
-        i = 1 + i 
-     return ("Your result is: " + str(fib2))
+
+    n = int(input("enter number in order fibonacci: "))
+    fib1 = fib2 = 1
+    for element in range(3, n + 1):
+      fib1, fib2 = fib2, fib1 + fib2
+    return fib2
+     
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
     n = 0
