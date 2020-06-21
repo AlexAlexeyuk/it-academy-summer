8cf6303215a5c60e283e12055fa70ce764329200
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-21 18:24:08 +0300

    Task4*

--- HomeWork_2/task4.py
+++ HomeWork_2/task4.py
@@ -1,13 +1,12 @@
-"""Посчитать количество строчных (маленьких) и прописных (больших) букв в
-    введенной строке. Учитывать только английские буквы.
-"""
 def count_letters(str_):
+    
     """Подсчет символов.
     :param str_: входная строка
     :return: кортеж. (low_number, up_number).
     low_number - количество строчных,
     up_number - количество пописных.
     """
+    
     str_ = input("Enter any string: ")
     low_number = 0
     up_number = 0
@@ -18,6 +17,7 @@ def count_letters(str_):
             if 'A' <= i <= 'Z':
                 up_number += 1
     return (low_number, up_number) 
+
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
     str_ = ''
