9c6dc2923d2834a9cc8f4137e66fb8d1ce34480b
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-23 18:06:23 +0300

    task7_adv_one_more

--- HomeWork_2/task7_adv.py
+++ HomeWork_2/task7_adv.py
@@ -3,6 +3,7 @@ of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Finish the solution so that it returns the sum of all the multiples
 of 3 or 5 below the number passed in.Note: If the number is
 a multiple of both 3 and 5, only count it once.'''
+
 number = 14
 
 i = 3
@@ -16,3 +17,15 @@ while j < number:
     n_five = n_five + j
     j += 5
 print(n_five + n_tree)
+
+#one more
+def square_digits(num):
+
+'''For example, if we run 9119 through the function, 
+811181 will come out, because 92 is 81 and 12 is 1.'''
+
+    lst = []
+    lst_ = [i for i in str(num)]
+    for i in lst_:
+        lst.append((str(int(i)**2)))
+    return int(''.join(lst))
