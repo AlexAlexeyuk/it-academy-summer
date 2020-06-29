fc034df51986f6e81e360cf6214b464dcac593ba
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-29 16:20:52 +0300

    Update task7_adv.py

--- HomeWork_2/task7_adv.py
+++ HomeWork_2/task7_adv.py
@@ -1,8 +1,10 @@
 '''If we list all the natural numbers below 10 that are multiples
+
 of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Finish the solution so that it returns the sum of all the multiples
 of 3 or 5 below the number passed in.Note: If the number is
-a multiple of both 3 and 5, only count it once.'''
+a multiple of both 3 and 5, only count it once.
+'''
 
 number = 14
 
@@ -21,8 +23,11 @@ print(n_five + n_tree)
 #one more
 def square_digits(num):
 
-'''For example, if we run 9119 through the function, 
-811181 will come out, because 92 is 81 and 12 is 1.'''
+
+    '''For example, if we run 9119 through the function, 
+
+    811181 will come out, because 92 is 81 and 12 is 1.
+    '''
 
     lst = []
     lst_ = [i for i in str(num)]
