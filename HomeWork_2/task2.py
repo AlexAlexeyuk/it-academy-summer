c88820f476964ac60e790579c0f9d89431af069c
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-29 16:14:22 +0300

    Update task2.py

--- HomeWork_2/task2.py
+++ HomeWork_2/task2.py
@@ -1,14 +1,15 @@
 def longest_word(str_):
 
     """Поиск самого длинного слова в предложении.
+
     :param str_: входная строка
     :return: строка. Самое длинное слово в предложении (в случае если их
     несколько, самое левое в строке).
-    в случае если"""
+    в случае если
+    """
 
     str_ = input("Enter some words: ")
-    return("the longest word is: "
-           + max(str_.strip("!#$%&'()*+, -./:;<=>?@[\]^_`{|}~'").split(), key = len))
+    return("the longest word is: " + max(str_.strip("!#$%&'()*+, -./:;<=>?@[\]^_`{|}~'").split(), key = len))
 
 if __name__ == '__main__':
     str_ = ''
