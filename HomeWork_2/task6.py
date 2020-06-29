89ae9d82f4818d334cf73630ca135abf44cb7169
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-06-29 16:19:05 +0300

    Update task6.py

--- HomeWork_2/task6.py
+++ HomeWork_2/task6.py
@@ -1,16 +1,19 @@
 def palindrom(n):
-    
-"""Поиск числа фибоначчи.
-:param n: Число.
-:return: Bool. True или False. 
-Является ли число палиндромом."""
+
+
+    """Поиск числа фибоначчи.
+
+    :param n: Число.
+    :return: Bool. True или False. 
+    Является ли число палиндромом.
+    """
 
     n = k = int(input("Enter any integer: "))
     n2 = 0
     while n > 0:
         left = n % 10; 
         n = n // 10; 
-        n2 = n2 * 10 ;
+        n2 = n2 * 10;
         n2 = n2 + left;
     if n2 == k:
         return True
