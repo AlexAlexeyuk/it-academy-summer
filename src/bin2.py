08e962060d841cc8143205d7145da5790b437ab4
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-07-29 19:25:09 +0300

    initial commit

---
+++ src/bin2.py
@@ -0,0 +1,15 @@
+def max_div(i):
+    """Вводится число найти его максимальный делитель,
+    являющийся степенью двойки. 10(2) 16(16), 12(4)
+    """
+    degree = 0
+    div = 1
+    while True:
+        new_div = 1 << degree
+        if not i % new_div:
+            degree += 1
+            div = new_div
+        else:
+            break
+    return div
+print(max_div(12))
