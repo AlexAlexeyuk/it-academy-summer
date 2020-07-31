a7ca196a7aa7b057e614298b2b548e84b34cfc14
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-07-31 11:39:15 +0300

    Update task5.py

--- src/task5.py
+++ src/task5.py
@@ -6,8 +6,8 @@
 
 students = [{input() for j in range(int(input()))}
             for i in range(int(input()))]
-known_by_everyone, known_by_someone = set.intersection(*students), \
-                                      set.union(*students)
+known_by_everyone = set.intersection(*students)
+known_by_someone = set.union(*students)
 print(len(known_by_everyone))
 print(known_by_everyone)
 print(len(known_by_someone))
