3284a18dc3b5e572f9b4573c8eb07c8ea0962dfe
AlexAlexeyuk <65073437+AlexAlexeyuk@users.noreply.github.com>
2020-07-31 11:41:26 +0300

    Update bin2.py

--- src/bin2.py
+++ src/bin2.py
@@ -3,6 +3,7 @@
 являющийся степенью двойки. 10(2) 16(16), 12(4)
 """
 
+
 def max_div(i):
     degree = 0
     div = 1
@@ -14,4 +15,6 @@ def max_div(i):
         else:
             break
     return div
+
+
 print(max_div(12))
